8244dd2e1cef0ec46e4d9e583a1aa192ca74c221
Magnus <3071258c@student.gla.ac.uk>
2026-02-19 21:30:48 +0000

    Chapter 10: Cookies and Sessions

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,36 @@ from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit.
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
@@ -46,11 +76,6 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
@@ -59,11 +84,21 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context=context_dict)
+    # Call the helper function to handle the cookies
+    visitor_cookie_handler(request)
+
+    # Get the number of visits from the session.
+    context_dict['visits'] = request.session['visits']
+
+    # Render the response and return it
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
-    return render(request, 'rango/about.html')
+    context_dict = {'author': 'Magnus'}
+
+    return render(request, 'rango/about.html', context=context_dict)
+
 
 @login_required
 def add_category(request):
