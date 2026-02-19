c35417f25e29d88e0dd2624e83b8b2e967888ea1
Magnus <3071258c@student.gla.ac.uk>
2026-02-19 15:28:07 +0000

    Chapter 5: models + admin + population script (up to 5.7)

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.template.defaultfilters import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+class Page(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
