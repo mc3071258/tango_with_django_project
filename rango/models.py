993121bfdf6b73d072e343ba41236c3517fb36f6
Magnus <3071258c@student.gla.ac.uk>
2026-02-19 15:49:37 +0000

    Chapter 5: complete end-of-chapter exercises + admin customisation (tests pass)

--- rango/models.py
+++ rango/models.py
@@ -4,11 +4,16 @@ from django.template.defaultfilters import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Category, self).save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
@@ -21,3 +26,4 @@ class Page(models.Model):
 
     def __str__(self):
         return self.title
+    
